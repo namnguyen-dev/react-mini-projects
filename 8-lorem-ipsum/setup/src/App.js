ee102187cba5988b65fb839a803bbebe5093cdcd
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-03-28 18:38:59 +0700

    complete react mini project no8-lorem ipsum

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(data.length);
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length - 1) {
+      amount = data.length - 1;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h2>lorem ipsum project setup</h2>
+      <form action="" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
