131b40bb0318cdd2b6c140cc12b2b7bbdb5779ce
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-03-30 19:31:28 +0700

    add increase dispatch

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -3,7 +3,18 @@ const reducer = (state, action) => {
         return {...state, cart: []}
     }
     if(action.type === 'REMOVE') {
-       return {...state, cart: state.cart.filter((cartItem)=> cartItem.id !== action.payload )} 
+      return{...state,cart:state.cart.filter((cardItem) => {
+          return cardItem.id !==action.payload
+      })}
+    }
+    if(action.type === 'INCREASE') {
+         let tempCart = state.cart.map(cardItem => {
+           if (cardItem.id === action.payload) {
+             return { ...cardItem, amount: cardItem.amount + 1 };
+           }
+           return cardItem;
+         });
+         return { ...state, cart: tempCart };
     }
 
     return state
