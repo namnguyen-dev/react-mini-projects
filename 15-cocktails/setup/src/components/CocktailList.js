5d10d08da0835ffd91f1439ac6c5777371d489e9
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-03-31 16:21:15 +0700

    add fetchDrinks and display CocktailList

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
-import { getByTitle } from '@testing-library/dom'
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
+import { getByTitle } from '@testing-library/dom';
 
 const CocktailList = () => {
-  const {cocktails, loading} = useGlobalContext()
- if (loading) {
-   return <Loading/>
- }
- if(cocktails.length<1) {
-   
-   return <h2 className='section-title'>
-     no cocktails matched your criteria
-   </h2>
- }
+  const { cocktails, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">no cocktails matched your criteria</h2>
+    );
+  }
   return (
-   
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item)=> {
+          return <Cocktail key={item.id} {...item}/>
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default CocktailList
+export default CocktailList;
