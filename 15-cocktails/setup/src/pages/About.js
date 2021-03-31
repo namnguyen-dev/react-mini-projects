ff1f115b8541a3df24f4635ae12ced330d171862
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-03-31 14:20:10 +0700

    add About and Error pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">
+        about us
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ab libero impedit! Autem quas, maiores sunt odit voluptatem numquam hic, veritatis necessitatibus unde reiciendis possimus vitae ipsa aspernatur magnam asperiores quo sit? Itaque illo molestiae eveniet dolores vel voluptatem totam!</p>
+    </section>
   )
 }
 
