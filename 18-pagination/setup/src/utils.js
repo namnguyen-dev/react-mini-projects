d3457834ac74db7a1c85c3ab3466f65dc1df3c17
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-02 16:41:03 +0700

    complete react mini project no18-pagination

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 11;
+    const newFollowers = new Array(Math.ceil(followers.length / itemsPerPage))
+      .fill()
+      .map(_ => followers.splice(0, itemsPerPage));
+      
+    return newFollowers
+    
+}
 
 export default paginate
