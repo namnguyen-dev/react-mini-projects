a2a09c4d8f7cb7d1454ee624ee32b8a907ff63ae
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-03 18:54:59 +0700

    set up SingleMovie component

--- 21-movie-db/setup/src/Movies.js
+++ 21-movie-db/setup/src/Movies.js
@@ -16,9 +16,9 @@ const Movies = () => {
       {movies.map(movie => {
         const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
         return (
-          <Link to={`/movies/:${id}`} key={id} className='movie'>
-            <article >
-              <img src={poster === 'N/A' ? url: poster} alt={title}/>
+          <Link to={`/movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
               <div className="movie-info">
                 <h4 className="title">{title}</h4>
                 <p>{year}</p>
