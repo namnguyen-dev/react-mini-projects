786df3390002af25162d25e43c9ca5a84c0d82ca
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-03 17:17:29 +0700

    set up Movie component

--- 21-movie-db/setup/src/Movies.js
+++ 21-movie-db/setup/src/Movies.js
@@ -1,21 +1,34 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useGlobalContext } from './context';
+import { Link } from 'react-router-dom';
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 
 const Movies = () => {
-  const {movies} = useGlobalContext()
+  const { movies, isLoading } = useGlobalContext();
   console.log(movies);
-  
-  return <section>
-    {movies.map((movie)=> {
-      const {Poster} = movie 
-      return <article>
-        <img src={Poster} alt=""/>
-      </article>
-    })}
-  </section>
-}
 
-export default Movies
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map(movie => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
+        return (
+          <Link to={`/movies/:${id}`} key={id} className='movie'>
+            <article >
+              <img src={poster === 'N/A' ? url: poster} alt={title}/>
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Movies;
