beb92a5e2428365e79873c0f2e576d6198801a88
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-05 10:50:11 +0700

    update Stories component

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -3,19 +3,31 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Stories = () => {
-  const {isLoading, hits} = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
   if (isLoading) {
     return <div className="loading"></div>;
   }
-  return <section className="stories">
-    {hits.map((story) => {
-      const {points, story_title, objectID,author, num_comments } = story
-      return <article className='story'>
-        <h1>{story_title}</h1>
-        <p>{points} points by {author} | {num_comments || null}comments</p>
-      </article>
-    })}
-  </section>
+  return (
+    <section className="stories">
+      {hits.map(story => {
+        const { points, story_title:title, objectID, author, num_comments,story_url:url } = story;
+        return (
+          <article className="story" key={objectID}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author} |</span> {num_comments} comments
+            </p>
+            <div>
+              <a href={url} className="read-link" target='_blank' rel='noopener noreferrer'>
+                read more
+              </a>
+              <button className="remove-btn">remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
