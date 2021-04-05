5cab0ab3295e4567e3bab9eb34e230e07906e23c
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-05 16:34:55 +0700

    display question

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
-import SetupForm from './SetupForm'
-import Loading from './Loading'
-import Modal from './Modal'
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
 function App() {
-  return <h2>quiz starter</h2>
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">correct answers : {correct}/{index}</p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{__html: question}} ></h2>
+          <div className="btn-contaainer">
+            {answers.map((answer, index)=> {
+              return <button className="answer-btn" dangerouslySetInnerHTML={{__html: answer}} key={index}></button>
+            } )}
+          </div>
+        </article>
+        <button className='next-question'>next question</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
